82e07fbf2c4a8fec65fc164ffc3fc98472da0112
Elissa Sosland and Kellie Olson <elissa@>
2016-03-22 14:56:22 -0700

    add clickable functionality to words

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,96 @@
 $(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#rjavascript").toggle();
+    $("#javascript").toggle();
+  });
 
+  $(".clickable2").click(function() {
+    $("#roperators").toggle();
+    $("#operators").toggle();
+  });
 
+  $(".clickable3").click(function() {
+    $("#rvariables").toggle();
+    $("#variables").toggle();
+  });
+
+  $(".clickable4").click(function() {
+    $("#rvnc").toggle();
+    $("#vnc").toggle();
+  });
+
+  $(".clickable5").click(function() {
+    $("#rfunctions").toggle();
+    $("#functions").toggle();
+  });
+
+  $(".clickable6").click(function() {
+    $("#rmethods").toggle();
+    $("#methods").toggle();
+  });
+
+  $(".clickable7").click(function() {
+    $("#rarguments").toggle();
+    $("#arguments").toggle();
+  });
+
+  $(".clickable8").click(function() {
+    $("#rparameters").toggle();
+    $("#parameters").toggle();
+  });
+
+  $(".clickable9").click(function() {
+    $("#rreturn").toggle();
+    $("#return").toggle();
+  });
+
+  $(".clickable10").click(function() {
+    $("#rcm").toggle();
+    $("#cm").toggle();
+  });
+
+  $(".clickable11").click(function() {
+    $("#rstrings").toggle();
+    $("#strings").toggle();
+  });
+
+  $(".clickable12").click(function() {
+    $("#rbooleans").toggle();
+    $("#booleans").toggle();
+  });
+
+  $(".clickable13").click(function() {
+    $("#rundefined").toggle();
+    $("#undefined").toggle();
+  });
+
+  $(".clickable14").click(function() {
+    $("#rNaN").toggle();
+    $("#NaN").toggle();
+  });
+
+  $(".clickable15").click(function() {
+    $("#resc").toggle();
+    $("#esc").toggle();
+  });
+
+  $(".clickable16").click(function() {
+    $("#ralert").toggle();
+    $("#alert").toggle();
+  });
+
+  $(".clickable17").click(function() {
+    $("#rcomments").toggle();
+    $("#comments").toggle();
+  });
+
+  $(".clickable18").click(function() {
+    $("#rjquery").toggle();
+    $("#jquery").toggle();
+  });
+
+  $(".clickable19").click(function() {
+    $("#rattributes").toggle();
+    $("#attributes").toggle();
+  });
 });
