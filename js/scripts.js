859fd279ec521b223cad1e3cc735eaedb4d2e9b7
Kellie Olson <kellie.a.olson@biola.edu>
2016-03-22 22:12:57 -0700

    Flashcards yo

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,26 @@
 $(document).ready(function() {
-  $(".clickable1").click(function() {
-    $("#rjavascript").toggle();
-    $("#javascript").toggle();
+
+  $("#previousCard").click(function() {
+    if ($(".flashcard-active").prev().length > 0) {
+      $(".flashcard-active").hide().removeClass("flashcard-active")
+        .prev().addClass("flashcard-active").show();
+    }
+
   });
 
-  $(".clickable2").click(function() {
-    $("#roperators").toggle();
-    $("#operators").toggle();
+  $("#nextCard").click(function() {
+    if ($(".flashcard-active").next().length > 0) {
+      $(".flashcard-active").hide().removeClass("flashcard-active")
+        .next().addClass("flashcard-active").show();
+    }
+
   });
 
-  $(".clickable3").click(function() {
-    $("#rvariables").toggle();
-    $("#variables").toggle();
+
+
+  $(".flashcard").click(function() {
+    $("h4, p").toggle();
   });
 
-  $(".clickable4").click(function() {
-    $("#rvnc").toggle();
-    $("#vnc").toggle();
-  });
 
-  $(".clickable5").click(function() {
-    $("#rfunctions").toggle();
-    $("#functions").toggle();
-  });
-
-  $(".clickable6").click(function() {
-    $("#rmethods").toggle();
-    $("#methods").toggle();
-  });
-
-  $(".clickable7").click(function() {
-    $("#rarguments").toggle();
-    $("#arguments").toggle();
-  });
-
-  $(".clickable8").click(function() {
-    $("#rparameters").toggle();
-    $("#parameters").toggle();
-  });
-
-  $(".clickable9").click(function() {
-    $("#rreturn").toggle();
-    $("#return").toggle();
-  });
-
-  $(".clickable10").click(function() {
-    $("#rcm").toggle();
-    $("#cm").toggle();
-  });
-
-  $(".clickable11").click(function() {
-    $("#rstrings").toggle();
-    $("#strings").toggle();
-  });
-
-  $(".clickable12").click(function() {
-    $("#rbooleans").toggle();
-    $("#booleans").toggle();
-  });
-
-  $(".clickable13").click(function() {
-    $("#rundefined").toggle();
-    $("#undefined").toggle();
-  });
-
-  $(".clickable14").click(function() {
-    $("#rNaN").toggle();
-    $("#NaN").toggle();
-  });
-
-  $(".clickable15").click(function() {
-    $("#resc").toggle();
-    $("#esc").toggle();
-  });
-
-  $(".clickable16").click(function() {
-    $("#ralert").toggle();
-    $("#alert").toggle();
-  });
-
-  $(".clickable17").click(function() {
-    $("#rcomments").toggle();
-    $("#comments").toggle();
-  });
-
-  $(".clickable18").click(function() {
-    $("#rjquery").toggle();
-    $("#jquery").toggle();
-  });
-
-  $(".clickable19").click(function() {
-    $("#rattributes").toggle();
-    $("#attributes").toggle();
-  });
 });
